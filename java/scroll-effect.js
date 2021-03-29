6c79ff5c8cfa124b1070901c10341cc9fa7da57d
Emanoel da Silva Almeida <emanoel2304@gmail.com>
2021-03-29 12:34:57 -0300

    Add files via upload

---
+++ java/scroll-effect.js
@@ -0,0 +1,11 @@
+
+        //scrolling effect
+
+        $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                $('nav').addClass('black');
+            }
+            else {
+                $('nav').removeClass('black');
+            }
+        })
